1a40f25eba66fd750f55982a92be8a3335532379
bunnycatto <steveflorenzm@gmail.com>
2023-11-01 16:40:05 +0800

    November Update
    - Nov 1, 2023 (did a major update to the web-site interface)

---
+++ assets/js/type-write-effect.js
@@ -0,0 +1,27 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".auto-type", {
+      strings: ["Hi!", "How are you?", "Good day!", "Happy Coding!",
+                "Click Explore!"],
+      typeSpeed: 100,
+      backSpeed: 90,
+      loop: true
+    });
+
+    var typed2 = new Typed(".code-type", {
+        strings: ["Code!", "Design!", "Develop!", "Troubleshoot!",
+        "drink coffee!"],
+        typeSpeed: 100,
+        backSpeed: 90,
+        loop: true
+    });
+
+    var typed3 = new Typed(".learn-type", {
+        strings: ["Learn", "Collaborate", "Curiousity", 
+        "Listen", "Creativity", "Communicate"],
+        typeSpeed: 100,
+        backSpeed: 85,
+        loop: true
+    });
+
+  });
